3a0a64fce3691fdd7c15427df36f51e0df125059
Farrel24342 <farrelnabil412@gmail.com>
2026-07-22 07:49:44 +0800

    ronald ngepel

--- studiKasus10.js
+++ studiKasus10.js
@@ -1,10 +1,8 @@
 const skor = [100, 450, 800, 150, 900];
 
-// Menggunakan perulangan for biasa untuk mendapatkan nomor Player (index + 1)
 for (let i = 0; i < skor.length; i++) {
   let rank = "";
 
-  // Percabangan if - else if - else
   if (skor[i] >= 800) {
     rank = "Mythic";
   } else if (skor[i] >= 500) {
