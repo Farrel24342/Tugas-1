9052bbade0b93ca6a1bfef6194f90f8c25df1921
Farrel24342 <farrelnabil412@gmail.com>
2026-07-26 19:53:47 +0800

    ronald ngepel

--- studiKasus6.js
+++ studiKasus6.js
@@ -1,10 +1,9 @@
 const umur = [8, 13, 17, 20, 25, 11];
 
-// Menggunakan perulangan for...of
-for (const u of umur) {
-  if (u >= 17) {
-    console.log(`Umur ${u}: Boleh Masuk`);
-  } else {
-    console.log(`Umur ${u}: Tidak Boleh Masuk`);
-  }
+for (let i = 0; i < umur.length; i++) {
+    if (umur[i] >= 17) {
+        console.log(`Umur ke-${umur[i]}: Boleh Masuk`);
+    } else {
+        console.log(`Umur ke-${umur[i]}: Tidak Boleh Masuk`);
+    }  
 }
